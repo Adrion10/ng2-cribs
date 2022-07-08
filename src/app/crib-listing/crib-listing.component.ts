00ffdcf8290c655df8dd8d77de7b32c6b826485d
Adrion10 <lacehila@gmail.com>
2022-07-08 23:41:45 +0200

    *ngFor ="let crib of cribs"

--- src/app/crib-listing/crib-listing.component.ts
+++ src/app/crib-listing/crib-listing.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { cribs } from './../data/cribs';
 
 @Component({
   selector: 'app-crib-listing',
   templateUrl: './crib-listing.component.html',
-  styleUrls: ['./crib-listing.component.css']
+  styleUrls: ['./crib-listing.component.css'],
 })
 export class CribListingComponent implements OnInit {
+  cribs: Array<any> = cribs;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit() {}
 }
